3bd48a594a84cc2cee4aa1484654de291a4ba15f
lennonzhao <iseezqq@163.com>
2018-09-24 15:22:48 +0800

    add

--- service/main.lua
+++ service/main.lua
@@ -1,11 +1,34 @@
 local skynet = require "skynet"
 
+local Person = class("Person")
+function Person:ctor(sex)
+    self.mAge = 1
+    self.mSex = sex
+end
+
+function Person:debug()
+    printInfo("self.mAge = %d", self.mAge)
+    printInfo("self.mSex = %d", self.mSex)
+end
+
+local Man = class("Man", Person)
+function Man:ctor(sex)
+    Man.super.ctor(self, sex)
+    self.mSex = sex
+end
+
 local function main()
     skynet.newservice("debug_console", 8081)
 
     printDebug('debug')
     printInfo('info')
     printError('error')
+
+    local person = Person.new(-1)
+    person:debug()
+    
+    local man = Man.new(2)
+    man:debug()
 
     -- 登陆服务
     -- local login = skynet.newservice("hall")
