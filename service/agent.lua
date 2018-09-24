584129adc67a127db086356d1f4b1129986101cf
lennonzhao <lennonzhao@github.com>
2018-09-25 06:04:11 +0800

    add

--- service/agent.lua
+++ service/agent.lua
@@ -55,12 +55,8 @@ skynet.register_protocol {
 	id = skynet.PTYPE_CLIENT,
 	unpack = function (msg, sz)
 		printInfo("unpack a new message", sz)
-		-- return netpack.filter(msg, sz)
-		-- printInfo("unpack", string.unpack(">s2", msg))
-		-- local data = string.unpack(">s2", msg)
-		-- dump(data, "111")
-		local data = netpack.filter(msg, sz)
-		return decode_msg(data, sz)
+		local str = skynet.tostring(msg, sz)
+		return decode_msg(str, sz)
 		-- return host:dispatch(msg, sz)
 	end,
 	dispatch = function (_, _, type, ...)
