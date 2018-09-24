8155e5e3857c9dd6a5d142e30f74e29070e90dc4
lennonzhao <iseezqq@163.com>
2018-09-24 16:09:00 +0800

    add

--- cfg/config.lua
+++ cfg/config.lua
@@ -10,22 +10,41 @@ bootstrap = "snlua bootstrap"	-- The service for bootstrap
 
 runconfig = "runconfig"
 start = "main"	-- main script
-
-lualoader = skynet_root  ..  "lualib/loader.lua"
-preload    = app_root .. "preload/preload.lua"
-
-luaservice = skynet_root .. "service/?.lua;" .. skynet_root .. "test/?.lua;" .. skynet_root .. "examples/?.lua"
-lua_path = skynet_root .. "lualib/?.lua;" .. skynet_root .. "lualib/?/init.lua;" .. skynet_root .. "examples/?.lua"
-lua_cpath = skynet_root  ..  "luaclib/?.so"
-snax = skynet_root .. "examples/?.lua;" .. skynet_root .. "test/?.lua"
 -- snax_interface_g = "snax_g"
-cpath = skynet_root .. "cservice/?.so"
+
+lualoader 	= 	skynet_root  ..  "lualib/loader.lua"
+preload    	= 	app_root .. "preload/preload.lua"
+
+luaservice 	= 	skynet_root .. "service/?.lua;" .. 
+				skynet_root .. "test/?.lua;" .. 
+				skynet_root .. "examples/?.lua"
+
+lua_path 	= 	skynet_root .. "lualib/?.lua;" .. 
+				skynet_root .. "lualib/?/init.lua;" .. 
+				skynet_root .. "examples/?.lua"
+
+lua_cpath 	= 	skynet_root  ..  "luaclib/?.so"
+snax 		= 	skynet_root .. "examples/?.lua;" .. 
+				skynet_root .. "test/?.lua"
+cpath 		= 	skynet_root .. "cservice/?.so"
 
 --our path
-luaservice = app_root .. "service/?.lua;" .. app_root .. "service/?/main.lua;" .. luaservice
-lua_path = app_root .. "preload/?.lua;" .. app_root .. "cfg/?.lua;" .. app_root .. "lualib/?.lua;" .. lua_path
-lua_cpath = app_root .. "luaclib/?.so;" .. lua_cpath
-cpath = app_root .. "cservice/?.so;" .. cpath
+luaservice  = 	app_root .. "service/?.lua;" .. 
+			 	app_root .. "service/?/main.lua;" .. 
+			 	app_root .. "service/agent/?.lua;" .. 
+			 	app_root .. "service/room/?.lua;" .. 
+			 	luaservice
+
+lua_path 	= 	app_root .. "preload/?.lua;" .. 
+				app_root .. "cfg/?.lua;" .. 
+				app_root .. "lualib/?.lua;" ..
+				lua_path
+
+lua_cpath 	= 	app_root .. "luaclib/?.so;" .. 
+				lua_cpath
+
+cpath 		= 	app_root .. "cservice/?.so;" .. 
+				cpath
 
 if $DAEMON then
 	logger = app_root .. "run/skynet.log"
