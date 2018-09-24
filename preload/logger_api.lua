a22c27b058a64c82dd89fceb424ca3c7065c0719
lennonzhao <iseezqq@163.com>
2018-09-24 15:17:46 +0800

    add

--- preload/logger_api.lua
+++ preload/logger_api.lua
@@ -13,9 +13,6 @@ local log_level = {
 }
 
 local defaultLevel = tonumber(skynet.getenv "log_default_lv") or log_level.LOG_DEBUG
-local daemon = skynet.getenv "daemon"
-
-skynet.error('daemon = ' .. tostring(daemon))
 local prefix = ""
 function LOG_PREFIX(pre)
   prefix = "[" .. pre .. "]"
@@ -27,7 +24,7 @@ local function logger(str, level, color)
     if level >= defaultLevel then
         local info = table.pack(...)
         info[#info+1] = "\x1b[0m"
-        skynet.error(string.format("%s%s%s", daemon and "" or color, prefix, str), table.unpack(info)) 
+        skynet.error(string.format("%s%s%s", color, prefix, str), table.unpack(info)) 
     end
   end
 end
