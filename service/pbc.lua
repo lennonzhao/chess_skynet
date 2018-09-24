c45b2c7892eea355704134ca75a381a049de7b4a
lennonzhao <iseezqq@163.com>
2018-09-24 11:55:28 +0800

    ad

--- service/pbc.lua
+++ service/pbc.lua
@@ -1,6 +1,6 @@
 local skynet = require "skynet"
 require "skynet.manager"
-local protobuf = require "protobuf"
+local pb = require "protobuf"
 
 local pb_files = {
 	"proto/common.pb",
@@ -11,34 +11,47 @@ local cmd = {}
 
 function cmd.init()
 	for _,v in ipairs(pb_files) do
-		protobuf.register_file(v)
+		pb.register_file(v)
 	end
 end
 
 function cmd.encode(msg_name, msg)
 	skynet.error("encode"..msg_name)
-	return protobuf.encode(msg_name, msg)
+	return pb.encode(msg_name, msg)
 end
 
 function cmd.decode(msg_name, data)
 	skynet.error("decode ".. msg_name.. " " .. type(data) .." " .. #data)
-	return protobuf.decode(msg_name, data)
+	return pb.decode(msg_name, data)
 end
 
 function cmd.test()
 	skynet.error("pbc test...")
-	local msg = {account = "name"}
-	skynet.error("encode")
-	local data = cmd.encode("Login.Login", msg)
-	skynet.error("decode"..#(data))
-	local de_msg = cmd.decode("Login.Login", data)
-	skynet.error(de_msg.account)
+
+	local data = pb.encode("hall.LoginReq", {
+		request = {
+			code = 0x1001,
+			api = 101,
+		},
+		basic = {
+
+		}
+	})
+	skynet.error("pb encode: " .. type(data))
+
+	local __basepb = pb.decode("hall.LoginReq", data)
+	if __basepb then
+		skynet.error(__basepb.request.code)
+		skynet.error(__basepb.request.api)
+	else
+		skynet.error("error")
+	end
 end
 
 skynet.start(function ()
 	skynet.error("init pbc...")
 	cmd.init()
-
+	cmd.test()
 	skynet.dispatch("lua", function (session, address, command, ...)
 		local f = cmd[command]
 		if not f then
