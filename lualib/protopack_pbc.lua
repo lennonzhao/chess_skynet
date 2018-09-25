7dd63ebf42ca5b8142b6c6f57b44576338b55daf
lennonzhao <iseezqq@163.com>
2018-09-25 14:35:27 +0800

    add

--- lualib/protopack_pbc.lua
+++ lualib/protopack_pbc.lua
@@ -56,7 +56,7 @@ function M.pack(gameId, pbName, msg, session)
 	local pbstr = _encode(pbName, msg)
 	local pblen = string.len(pbstr)
 	--组成发送字符串 前面两个字节表示包的总长度
-	local str = string.pack(">I2I4I2I4s2", pblen + 10, pblen + 6, gameId, session, pbstr)
+	local str = string.pack(">HI4I2I4s2", pblen + 10 + 2, pblen + 6, gameId, session, pbstr)
 
 	print("send:", bin2hex(str), string.len(str), pblen)
 	print(string.format("send: gameId(%d) cmd(0x%04x) pbName(%s) msg->%s session(%d)", gameId, msg.request.code, pbName, msg, session))
