72a6c6560dc711340c1eae07ca09c98027c05111
lennonzhao <lennonzhao@github.com>
2018-09-27 12:39:29 +0800

    add

--- service/agent.lua
+++ service/agent.lua
@@ -22,6 +22,75 @@ local function recv_request(session, source, cmd, msg, pbName, check)
 	protopack.dump(pbName, msg, string.format("0x%04x", cmd))
 	if cmd == Command.Login then
 		printInfo("玩家请求登录游戏，返回登录成功的包")
+		send_request(Command.Login, {
+            activityUrl = "http://proxy.zg915.com/public/activity_103",
+            broadcast = "",
+            builtCode = 0,
+            builtCodes = {},
+            config = {
+                bindProxyAward = 5,
+                worldchatCost = 0
+            },
+            downloadUrl = "",
+            gameInfos = { {
+                    flag = 0,
+                    id = 12000,
+                    name = "经典斗牛",
+                    online = "",
+                    type = 21
+                }, {
+                    flag = 0,
+                    id = 10900,
+                    name = "四川两人两房",
+                    online = "",
+                    type = 10
+                } },
+            inviteUrl = "",
+            ip = 1378703217,
+            isAgent = false,
+            isBinded = 0,
+            noticeUrl = "",
+            playCode = 0,
+            proxyUrl = "",
+            result = {
+                gameId = 40000,
+                message = "",
+                request = {
+                    api = 0,
+                    code = 1
+                },
+                status = 0
+            },
+            shareUrl = "http://proxy.zg915.com/public/authorize",
+            thirdInfo = "",
+            updateUrl = "",
+            userInfo = {
+                firstPayStatus = 0,
+                id = 202779,
+                isRegister = 0,
+                lastLoginTime = "",
+                playerInfo = {
+                    boxMoney = 0,
+                    diamond = 72,
+                    drawTimes = 0,
+                    headUrl = "",
+                    headUrlSmall = "",
+                    id = 202779,
+                    jsonStr = "",
+                    loseTimes = 0,
+                    money = 2000,
+                    nick = "mx4",
+                    sex = 0,
+                    signature = "",
+                    vipLevel = 0,
+                    winTimes = 0
+                },
+                session = ""
+            },
+            userType = 0,
+            voiceUrl = "http://voice.bcewo.cn:7000",
+            wechatPay = true
+        })
 	end
 end
 
