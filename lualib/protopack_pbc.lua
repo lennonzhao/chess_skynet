117d7dc259be68d2cee5277c43149aa25cd03271
lennonzhao <lennonzhao@github.com>
2019-05-03 00:03:32 +0800

    1

--- lualib/protopack_pbc.lua
+++ lualib/protopack_pbc.lua
@@ -112,7 +112,7 @@ function M.unpack(data)
     local buf_head, buf_body, ch_end = string.unpack(">s2s2c1", data)
     local msg_head = _decode("common.MsgHead", buf_head)
     local msg_body = _decode(msg_head.pbName, buf_body)
-    return msg_head.msgname, msg_body, msg_head.status
+    return msg_head.pbName, msg_body, msg_head.status
 end
 
 
